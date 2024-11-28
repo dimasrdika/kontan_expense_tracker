b6f1266ddc0c73b86859d8503eacd3ef78c45a22
dimasrdika <dimasrachmadika@gmail.com>
2024-11-28 20:36:55 +0700

    add spinner loader

---
+++ app/_components/loaderspinner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CoinFlipLoader: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="coin-loader"></div>
+    </div>
+  );
+};
+
+export default CoinFlipLoader;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
 "use client";
-import Image from "next/image";
+
+import React, { useState, useEffect } from "react";
 import Header from "./_components/header";
 import Hero from "./_components/hero";
+import LoaderSpinner from "./_components/loaderspinner";
 
 export default function Home() {
+  // State untuk menentukan apakah konten telah dimuat
+  const [loading, setLoading] = useState(true);
+
+  // Gunakan useEffect untuk mengatur loading selama 2 detik
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Setelah 2 detik, set loading ke false
+    }, 2000); // 2000ms = 2 detik
+
+    return () => clearTimeout(timer); // Bersihkan timer jika komponen dibongkar
+  }, []);
+
   return (
-    <>
-      <Header />
-      <Hero />
-    </>
+    <div className="overflow-x-hidden min-h-screen">
+      {loading ? (
+        <LoaderSpinner /> // Tampilkan spinner selama loading
+      ) : (
+        <>
+          <Header />
+          <Hero />
+        </>
+      )}
+    </div>
   );
 }
